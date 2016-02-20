bb135b14dce862fd333f2ab81245d8230fe8f701
xemlock <xemlock@gmail.com>
2016-02-20 16:56:25 +0100

    Resigned from class_alias as it adds unnecessary complexity

--- bootstrap.php
+++ bootstrap.php
@@ -4,11 +4,7 @@ if (!class_exists('SplEnum')) {
     if (!class_exists('Polyfill_SplEnum')) {
         require_once dirname(__FILE__) . '/src/Polyfill/SplEnum.php';
     }
-    if (function_exists('class_alias')) {
-        class_alias('Polyfill_SplEnum', 'SplEnum');
-    } else {
-        class SplEnum extends Polyfill_SplEnum
-        {
-        }
+    class SplEnum extends Polyfill_SplEnum
+    {
     }
 }
